3b1f595cca36ec6c207ccdc95fca2ea5bd567487
AleksMash <amashukov@yandex.ru>
2022-01-12 02:17:41 +0600

    Юнит 2.9

--- NewsPaper/News_Portal/News_Portal/urls.py
+++ NewsPaper/News_Portal/News_Portal/urls.py
@@ -1,4 +1,4 @@
-"""NewsPaper URL Configuration
+"""News_Portal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
