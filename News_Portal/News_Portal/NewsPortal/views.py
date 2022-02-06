cfbb5b07943356887783e74fd7783a03756a5be5
AleksMash <amashukov@yandex.ru>
2022-02-06 17:58:25 +0600

    D4.2 task. Part 1

--- News_Portal/News_Portal/NewsPortal/views.py
+++ News_Portal/News_Portal/NewsPortal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView  # импортируем класс,
+from django.views.generic import ListView, DetailView  # импортируем класс
+from django.views import View  # импортируем простую вьюшку
 from .models import Post
+from .filters import NewsFilter
+from django.core.paginator import Paginator  # импортируем класс, позволяющий удобно осуществлять постраничный вывод
 
 # который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
 # Create your views here.
@@ -19,3 +22,14 @@ class NewDetail(DetailView):
     model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
     template_name = 'SingleNew.html'  # название шаблона будет SingleNew.html
     context_object_name = 'single_new'  # название объекта. в нём будет
+
+class NewsFiltered(ListView):
+    model = Post
+    template_name = 'news_filtered.html'
+    context_object_name = 'news'
+    ordering = ['-date_added']
+
+    def get_context_data(self, **kwargs):  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса (привет, полиморфизм, мы скучали!!!)
+        context = super().get_context_data(**kwargs)
+        context['news'] = NewsFilter(self.request.GET, queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        return context
