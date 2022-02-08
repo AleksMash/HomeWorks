986a123f9f418ae342954d463d67b35923ed7b6e
AleksMash <90156264+AleksMash@users.noreply.github.com>
2022-02-09 02:09:50 +0600

    Delete temp.py

--- News_Portal/temp.py
+++
@@ -1,8 +0,0 @@
-import re, random
-
-pattern=r"\bсук[аи]+\w*\b|\bбляд\w*\b|\b\w*ху[ий]\w*\b|\b\w*[её]ба[тнл]\w*\b|\b\w*пизд\w*\b"
-
-p=re.compile(pattern,re.IGNORECASE)
-s1 = p.sub('***', "сука похуй блядь ебанная")
-
-print(s1)
